e0d0bd89b5dd8feff3fbc79d7603352365167575
邓怀诚 <530465852@qq.com>
2019-04-01 13:42:58 +0800

    list-group组件border修改

--- src/libs/list-group/index.js
+++ src/libs/list-group/index.js
@@ -2,7 +2,7 @@
  * @Author: denghuaicheng
  * @Date: 2019-03-18 21:06:21
  * @Last Modified by: denghuaicheng
- * @Last Modified time: 2019-03-18 21:09:17
+ * @Last Modified time: 2019-04-01 13:40:06
  * summary: 数据展示： list-group
  */
 
@@ -15,7 +15,7 @@ import './index.css';
 class ListGroup extends React.Component {
   render() {
     return (
-      <div className="dmui-group-list">
+      <div className="dmui-group-list border-mobile--bm border-mobile--tp">
         <Slot parentComponents={this} />
       </div>
     );
